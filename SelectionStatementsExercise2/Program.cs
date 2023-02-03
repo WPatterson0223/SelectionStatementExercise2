3ab51b83ca058f8c0426f60c306fabb4196d084b
Will Patterson <wlp223@aol.com>
2023-02-03 13:23:04 -0500

    exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string retry;
+            do
+            {
+                retry = "no";
+                Console.WriteLine("What is your favorite school subject?");
+                var favSubject = Console.ReadLine();
+
+                switch (favSubject)
+                {
+                    case "math":
+                    case "Math":
+                        Console.WriteLine("Math is Great!");
+                        break;
+                    case "english":
+                    case "English":
+                        Console.WriteLine("English is fun!");
+                        break;
+                    case "history":
+                    case "History":
+                        Console.WriteLine("Some history is fun.");
+                        break;
+                    case "science":
+                    case "Science":
+                        Console.WriteLine("Sience is awful.");
+                        break;
+                    case "biomimicry":
+                    case "Biomimicry":
+                        Console.WriteLine("I don't know what that is.");
+                        break;
+                    default:
+                        Console.WriteLine("Don't break my code! Do you want to enter a real subject?");
+                        retry = Console.ReadLine();
+                        break;
+                }
+            }
+            while (retry != "no");
         }
     }
 }
